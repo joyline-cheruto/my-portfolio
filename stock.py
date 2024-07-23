35cf77eff46168b8a94f1cacfe52e0996d6fa299
joyline-cheruto <joycheruto832@gmail.com>
2024-07-23 19:20:59 +0300

    Simple Stock Price App

--- stock.py
+++ stock.py
@@ -8,7 +8,6 @@ Shown are the stock **closing price** and ***volume*** of Google!
 
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'GOOGL'
 #get data on this ticker
@@ -18,6 +17,11 @@ tickerDf = tickerData.history(period='1d', start='2022-5-31', end='2024-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
-
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
